5f9ed349690bcb748e7fece88db8a40ca201c94d
ashishcloudlearner-dev <ashishcloudlearner@gmail.com>
2026-03-02 11:06:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,12 @@
-resource "google_compute_instance" "whizlabs" {
-  name         = var.instance_name
-  machine_type = var.instance_type
-  zone         = var.instance_zone
-  tags         = ["http-server"]
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = var.disk_size
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-
-  metadata_startup_script = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-echo 'Hello from Whizlabs' | sudo tee /var/www/html/index.html
-sudo service apache2 start
-EOF
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
-resource "google_compute_firewall" "http-server" {
-  name    = "allow-http"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
+############ Creating FIFO Queue ############
+resource "aws_sqs_queue" "queue" {
+  name = "MyWhizQueue.fifo"
+  fifo_queue = true
+  content_based_deduplication = true
 }
