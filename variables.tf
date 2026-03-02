d80ded052479553c9c980845d85d9ace19cc3772
ashishcloudlearner-dev <ashishcloudlearner@gmail.com>
2026-03-02 11:06:53 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,9 @@
-variable "instance_zone" {
-  description = "Zone in which instance will be created. Cannot be changed later."
-  type        = string
-  default     = "us-central1-a"
+variable "access_key" {
+    description = "Access key to AWS console"
 }
-
-variable "instance_name" {
-  description = "Name of the Web Server Instance."
-  type        = string
-  default     = "whizlabs-instance"
+variable "secret_key" {
+    description = "Secret key to AWS console"
 }
-
-variable "instance_type" {
-  description = "Type of Instance to be created."
-  type        = string
-  default     = "n1-standard-1"
-}
-
-variable "disk_size" {
-  description = "Boot disk size of the instance."
-  type        = string
-  default     = "10"
+variable "region" {
+    description = "AWS region"
 }
